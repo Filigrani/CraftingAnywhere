5d3e6d795f0bfd5488230107bac191be2374db42
Cass Agius <cassanda.agius@gmail.com>
2021-05-20 10:02:40 +1000

    Add project files.

---
+++ src/Patches.cs
@@ -0,0 +1,141 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace CraftingAnywhere
+{
+    internal static class Patches
+    {
+
+        [HarmonyPatch(typeof(Panel_Crafting), "ItemPassesFilter")]
+        private static class ChangeCraftingLocation
+        {
+            internal static void Postfix(BlueprintItem bpi)
+            {
+                if (Settings.options.arrowAnywhere && bpi?.m_CraftedResult?.name == "GEAR_Arrow")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.arrowheadAnywhere && bpi?.m_CraftedResult?.name == "GEAR_ArrowHead")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.arrowShaftAnywhere && bpi?.m_CraftedResult?.name == "GEAR_ArrowShaft")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.bearskinBedrollAnywhere && bpi?.m_CraftedResult?.name == "GEAR_BearSkinBedRoll")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.bearskinCoatAnywhere && bpi?.m_CraftedResult?.name == "GEAR_BearSkinCoat")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.bedrollAnywhere && bpi?.m_CraftedResult?.name == "GEAR_BedRoll")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.bowAnywhere && bpi?.m_CraftedResult?.name == "GEAR_Bow")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.bulletAnywhere && bpi?.m_CraftedResult?.name == "GEAR_Bullet")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.patchworkBlanketAnywhere && bpi?.m_CraftedResult?.name == "GEAR_ClothSheet")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.deerskinBootsAnywhere && bpi?.m_CraftedResult?.name == "GEAR_DeerSkinBoots")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.deerskinPantsAnywhere && bpi?.m_CraftedResult?.name == "GEAR_DeerSkinPants")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.gunpowderAnywhere && bpi?.m_CraftedResult?.name == "GEAR_GunpowderCan")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.improvisedHatchetAnywhere && bpi?.m_CraftedResult?.name == "GEAR_HatchetImprovised")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.hookAnywhere && bpi?.m_CraftedResult?.name == "GEAR_Hook")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.improvisedKnifeAnywhere && bpi?.m_CraftedResult?.name == "GEAR_KnifeImprovised")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.lineAnywhere && bpi?.m_CraftedResult?.name == "GEAR_Line")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.moosehideSatchelAnywhere && bpi?.m_CraftedResult?.name == "GEAR_MooseHideBag")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.moosehideCloakAnywhere && bpi?.m_CraftedResult?.name == "GEAR_MooseHideCloak")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.rabbitskinHatAnywhere && bpi?.m_CraftedResult?.name == "GEAR_RabbitskinHat")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.rabbitskinMittsAnywhere && bpi?.m_CraftedResult?.name == "GEAR_RabbitSkinMittens")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.revolverAmmunitionAnywhere && bpi?.m_CraftedResult?.name == "GEAR_RevolverAmmoSingle")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.rifleAmmunitionAnywhere && bpi?.m_CraftedResult?.name == "GEAR_RifleAmmoSingle")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.snareAnywhere && bpi?.m_CraftedResult?.name == "GEAR_Snare")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.waterskinAnywhere && bpi?.m_CraftedResult?.name == "GEAR_Waterskin")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+
+                else if (Settings.options.wolfskinCoatAnywhere && bpi?.m_CraftedResult?.name == "GEAR_WolfSkinCape")
+                {
+                    bpi.m_RequiredCraftingLocation = CraftingLocation.Anywhere;
+                }
+            }
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,126 @@
+﻿using Harmony;
+using UnityEngine;
+using ModSettings;
+
+namespace CraftingAnywhere
+{
+	internal class CraftingAnywhereSettings : JsonModSettings
+	{
+		[Section("Ammunition Workbench Items")]
+		[Name("Bullet")]
+		[Description("Craft anywhere")]
+		public bool bulletAnywhere = false;
+
+		[Name("Gunpowder")]
+		[Description("Craft anywhere")]
+		public bool gunpowderAnywhere = false;
+
+		[Name("Revolver Ammunition")]
+		[Description("Craft anywhere")]
+		public bool revolverAmmunitionAnywhere = false;
+
+		[Name("Rifle Ammunition")]
+		[Description("Craft anywhere")]
+		public bool rifleAmmunitionAnywhere = false;
+
+
+
+		[Section("Forge Items")]
+		[Name("Arrowhead")]
+		[Description("Craft anywhere")]
+		public bool arrowheadAnywhere = false;
+
+		[Name("Improvised Hatchet")]
+		[Description("Craft anywhere")]
+		public bool improvisedHatchetAnywhere = false;
+
+		[Name("Improvised Knife")]
+		[Description("Craft anywhere")]
+		public bool improvisedKnifeAnywhere = false;
+
+
+
+		[Section("Workbench Items")]
+		[Name("Arrow")]
+		[Description("Craft anywhere")]
+		public bool arrowAnywhere = false;
+
+		[Name("Arrow Shaft")]
+		[Description("Craft anywhere")]
+		public bool arrowShaftAnywhere = false;
+
+		[Name("Bearskin Bedroll")]
+		[Description("Craft anywhere")]
+		public bool bearskinBedrollAnywhere = false;
+
+		[Name("Bearskin Coat")]
+		[Description("Craft anywhere")]
+		public bool bearskinCoatAnywhere = false;
+
+		[Name("Bedroll (BlanketMod)")]
+		[Description("Craft anywhere")]
+		public bool bedrollAnywhere = false;
+
+		[Name("Deerskin Boots")]
+		[Description("Craft anywhere")]
+		public bool deerskinBootsAnywhere = false;
+
+		[Name("Deerskin Pants")]
+		[Description("Craft anywhere")]
+		public bool deerskinPantsAnywhere = false;
+
+		[Name("Hook")]
+		[Description("Craft anywhere")]
+		public bool hookAnywhere = false;
+
+		[Name("Line")]
+		[Description("Craft anywhere")]
+		public bool lineAnywhere = false;
+
+		[Name("Moose-Hide Cloak")]
+		[Description("Craft anywhere")]
+		public bool moosehideCloakAnywhere = false;
+
+		[Name("Moose-Hide Satchel")]
+		[Description("Craft anywhere")]
+		public bool moosehideSatchelAnywhere = false;
+
+		[Name("Patchwork Blanket (BlanketMod)")]
+		[Description("Craft anywhere")]
+		public bool patchworkBlanketAnywhere = false;
+
+		[Name("Rabbitskin Hat")]
+		[Description("Craft anywhere")]
+		public bool rabbitskinHatAnywhere = false;
+
+		[Name("Rabbitskin Mitts")]
+		[Description("Craft anywhere")]
+		public bool rabbitskinMittsAnywhere = false;
+
+		[Name("Snare")]
+		[Description("Craft anywhere")]
+		public bool snareAnywhere = false;
+
+		[Name("Survival Bow")]
+		[Description("Craft anywhere")]
+		public bool bowAnywhere = false;
+
+		[Name("Waterskin (Better Water Management)")]
+		[Description("Craft anywhere")]
+		public bool waterskinAnywhere = false;
+
+		[Name("Wolfskin Coat")]
+		[Description("Craft anywhere")]
+		public bool wolfskinCoatAnywhere = false;
+	}
+
+	internal static class Settings
+	{
+		public static CraftingAnywhereSettings options;
+		public static void OnLoad()
+		{
+			options = new CraftingAnywhereSettings();
+			options.AddToModSettings("Crafting Anywhere");
+		}
+	}
+}
